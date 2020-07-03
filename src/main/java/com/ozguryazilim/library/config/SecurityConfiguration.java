c431532bceaff303cd625424309eb8091f3fc75a
Ufuk Güler <ufukglr@yandex.com>
2020-07-04 02:06:05 +0300

    typo

--- src/main/java/com/ozguryazilim/library/config/SecurityConfiguration.java
+++ src/main/java/com/ozguryazilim/library/config/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login")
+                    .logoutSuccessUrl("/login?logout")
                 .invalidateHttpSession(true)
                 .permitAll();
     }
