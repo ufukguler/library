f377822ee2bd83a31ab8e88805c15d79ec8d2b47
Ufuk Güler <ufukglr@yandex.com>
2020-07-04 00:48:52 +0300

    User authentication with JDBC and MySQL

---
+++ src/main/java/com/ozguryazilim/library/model/User.java
@@ -0,0 +1,90 @@
+package com.ozguryazilim.library.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user")
+public class  User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String mail;
+    private boolean active;
+    private String roles;
+
+    public User(){}
+
+    public User(String username, String password, String mail, boolean active, String roles) {
+        this.username = username;
+        this.password = password;
+        this.mail = mail;
+        this.active = active;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", mail='" + mail + '\'' +
+                ", active=" + active +
+                ", roles='" + roles + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/ozguryazilim/library/repository/UserRepo.java
+++ src/main/java/com/ozguryazilim/library/repository/UserRepo.java
@@ -2,9 +2,11 @@ package com.ozguryazilim.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.ozguryazilim.library.entity.User;
+import com.ozguryazilim.library.model.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
