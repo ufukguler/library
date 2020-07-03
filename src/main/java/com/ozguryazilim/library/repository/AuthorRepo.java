18e1584c175d33f0d93dd3b5031a71f175a3f045
Ufuk Güler <ufukglr@yandex.com>
2020-07-03 13:10:01 +0300

    update queries for author & publisher

--- src/main/java/com/ozguryazilim/library/repository/AuthorRepo.java
+++ src/main/java/com/ozguryazilim/library/repository/AuthorRepo.java
@@ -1,9 +1,18 @@
 package com.ozguryazilim.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.ozguryazilim.library.entity.Author;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface AuthorRepo extends JpaRepository<Author, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "update author set name= :name,comment= :comment where id = :id", nativeQuery = true)
+    int updateAuthor(Long id,String name, String comment);
 }
--- src/main/java/com/ozguryazilim/library/repository/PublisherRepo.java
+++ src/main/java/com/ozguryazilim/library/repository/PublisherRepo.java
@@ -1,9 +1,17 @@
 package com.ozguryazilim.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.ozguryazilim.library.entity.Publisher;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface PublisherRepo extends JpaRepository<Publisher, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "update publisher set name= :name,comment= :comment where id = :id", nativeQuery = true)
+    int updatePublisher(Long id,String name, String comment);
 }
