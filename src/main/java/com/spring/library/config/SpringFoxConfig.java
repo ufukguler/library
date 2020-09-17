20ea3acc78e62d3412aad37159dec71177250371
Ufuk Güler <ufukglr@yandex.com>
2020-09-17 20:52:21 +0300

    Update SpringFoxConfig.java

--- src/main/java/com/spring/library/config/SpringFoxConfig.java
+++ src/main/java/com/spring/library/config/SpringFoxConfig.java
@@ -16,7 +16,7 @@ public class SpringFoxConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/api/**"))
+                .paths(PathSelectors.any())
                 .build();
     }
 }
